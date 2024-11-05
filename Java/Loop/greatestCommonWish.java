5003436bf90a2f5f6fa3ac55d8c9747184bbe1ab
pacdug <buitiendung091003@gmail.com>
2024-11-05 14:59:11 +0700

    UCLN

---
+++ Java/Loop/greatestCommonWish.java
@@ -0,0 +1,32 @@
+package Loop;
+
+import java.util.Scanner;
+import java.math.MathContext;
+
+public class greatestCommonWish {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int b = sc.nextInt();
+
+        a= Math.abs(a);
+        b= Math.abs(b);
+
+        if(a==0 || b==0){
+            System.out.println("No greatest common factor");
+        }
+
+        while (a!=b){
+            if(a>b){
+                a = a - b;
+            }else{
+                b = b - a;
+            }
+        }
+        System.out.println("The greatest common factor is "+a);
+    }
+}
---
+++ Java/Loop/loanCalculation.java
@@ -0,0 +1,25 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class loanCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the amount of loan :");
+        double amount = sc.nextDouble();
+
+        System.out.println("Enter the month :");
+        int month = sc.nextInt();
+
+        System.out.println("Enter annual interest rate in percentage:");
+        double interestRate = sc.nextDouble();
+
+        double totalInterest = 0;
+        for(int i = 0; i < month; i++){
+            totalInterest += amount * (interestRate/100)/12 * month;
+        }
+
+        System.out.println("Total of interest: " + totalInterest);
+    }
+}
