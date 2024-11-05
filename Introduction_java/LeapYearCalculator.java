630cc0768309b9f9aff8722e7eff36813a126263
pacdug <buitiendung091003@gmail.com>
2024-11-05 09:10:20 +0700

    thuchanh6

---
+++ Introduction_java/LeapYearCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter year:");
+        int year = sc.nextInt();
+
+        String a ;
+        if(year%4==0 && year%100!=0 || year%400==0 && year%100 == 0){
+            a = "leap year";
+        } else {
+            a = "not leap year";
+        }
+
+        System.out.println(a);
+    }
+}
