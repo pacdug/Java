ebddb54f041b6baea8099d5724074e0c4ae84117
pacdug <buitiendung091003@gmail.com>
2024-11-05 09:20:44 +0700

    thuchanh7

---
+++ Introduction_java/BodyMassIndex.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter width : ");
+        int weight = sc.nextInt();
+        System.out.println("Enter height : ");
+        int height = sc.nextInt();
+
+        float bmi = weight / (height * height);
+
+        String mess;
+        if (bmi < 18.5) {
+            mess = "underweight";
+            System.out.println(mess);
+        } else if (bmi >= 18.5 && bmi < 25) {
+            mess = "normal";
+            System.out.println(mess);
+        } else if (bmi >= 25 && bmi < 30) {
+            mess = "Overweight";
+            System.out.println(mess);
+        }else if (bmi >= 30) {
+            mess = "Obese";
+            System.out.println(mess);
+        }
+
+    }
+}
