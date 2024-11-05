708a17724500e3c016a64727da678e1bca3aa721
pacdug <buitiendung091003@gmail.com>
2024-11-05 08:53:48 +0700

    thuchanh4

---
+++ Introduction_java/LinearEquationResolver.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        int a , b , c , x;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a :");
+        a = sc.nextInt();
+        System.out.println("Enter b :");
+        b = sc.nextInt();
+        System.out.println("Enter c :");
+        c = sc.nextInt();
+
+        if (b==c){
+            x = 0 ;
+        } else {
+            x = (c-b) / a ;
+        }
+
+        System.out.println("x = " + x);
+
+    }
+}
