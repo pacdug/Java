f30cfee826f55b4c816307bae23981805e814121
pacdug <buitiendung091003@gmail.com>
2024-11-07 16:44:46 +0700

    Convert_Temperature

---
+++ Array/src/ConvertTemperature/convertTemperature.java
@@ -0,0 +1,13 @@
+package ConvertTemperature;
+
+public class convertTemperature {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
+
+
