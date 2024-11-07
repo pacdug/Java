866e9ab835e300d3329b79f3b00b5680f8fa95a0
pacdug <buitiendung091003@gmail.com>
2024-11-07 11:30:01 +0700

    StopWatch

---
+++ Class/src/StopWatchAssignment/stopWatch.java
@@ -0,0 +1,30 @@
+package StopWatchAssignment;
+
+public class stopWatch {
+    private long startTime;
+    private long stopTime;
+
+    public stopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getStopTime() {
+        return stopTime;
+    }
+
+    public void Start() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void Stop() {
+        this.stopTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return stopTime - startTime;
+    }
+}
