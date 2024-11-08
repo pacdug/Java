396dad611bcb551c76e245f8bc017d9ee8e8ad6f
pacdug <buitiendung091003@gmail.com>
2024-11-08 16:42:55 +0700

    Class_Contruction

---
+++ AcessModifier_StaticMethod/src/ClassConstruction/Main.java
@@ -0,0 +1,16 @@
+package ClassConstruction;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+
+
+        student1.setName("Nguyen Van A");
+        student1.setClasses("12A1");
+
+
+        student1.displayStudentInfo();
+    }
+}
+
