d1eab56c194db2313f2cce632c78fc37932c63b1
pacdug <buitiendung091003@gmail.com>
2024-11-19 09:06:15 +0700

    Illustration_BubbleSort

--- floating_Bubble_Arrangement/src/foamy_Arrangement/BubbleSort.java
+++ floating_Bubble_Arrangement/src/foamy_Arrangement/BubbleSort.java
@@ -7,7 +7,7 @@ public class BubbleSort {
         boolean needNextPass = true;
         for (int k = 1; k < list.length && needNextPass; k++) {
             needNextPass = false;
-            for (int i = 0; i < list.length - k; i++) {
+            for(int i = 0; i < list.length - k; i++) {
                 if (list[i] > list[i + 1]) {
                     int temp = list[i];
                     list[i] = list[i + 1];
