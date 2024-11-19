6ac23d549d6534bf290282bbf939bb2788104b3a
pacdug <buitiendung091003@gmail.com>
2024-11-19 09:20:38 +0700

    selection_Sort

---
+++ floating_Bubble_Arrangement/src/Selection_Sort/SelectionSort.java
@@ -0,0 +1,31 @@
+package Selection_Sort;
+
+public class SelectionSort {
+    static double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+
+    public static void selectionSort(double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            double min = list[i];
+            int minIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[j] < min) {
+                    min = list[j];
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                list[minIndex] = list[i];
+                list[i] = min;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + ",  ");
+        }
+    }
+}
